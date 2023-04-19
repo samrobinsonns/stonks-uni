03810534472bb142cadcaa52ba9323050ffe04bb
Jack Topping <62509008+Jacktopping56@users.noreply.github.com>
2023-04-19 07:04:27 +0100

    Updated stocksCard.jsx so it allows the user to remove the stock and it also caches the webpage so when the user refreshes it stays with the webpage

--- resources/js/components/stocksCard.jsx
+++ resources/js/components/stocksCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {
     MDBCard,
@@ -7,38 +7,55 @@ import {
     MDBCardText,
     MDBCardImage,
     MDBBtn,
-    MDBInput
+    MDBInput,
+    MDBIcon
 } from 'mdb-react-ui-kit';
+
 
 export default function StocksCard() {
     const [search, setSearch] = useState('');
-    const [stockInfo, setStockInfo] = useState({price: null, error: null});
+    const [stockInfo, setStockInfo] = useState({ price: null, error: null });
     const [pinnedStocks, setPinnedStocks] = useState([]);
+
+    useEffect(() => {
+        const cachedStocks = localStorage.getItem('pinnedStocks');
+        if (cachedStocks) {
+            setPinnedStocks(JSON.parse(cachedStocks));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('pinnedStocks', JSON.stringify(pinnedStocks));
+    }, [pinnedStocks]);
 
     const handleSearch = async () => {
         try {
             const response = await fetch(`http://localhost:8000/stocks/${search}`);
             const data = await response.json();
             console.log(search);
-            setStockInfo({price: data.price, error: null});
+            setStockInfo({ price: data.price, error: null });
         } catch (error) {
-            setStockInfo({price: null, error: 'Error retrieving stock price'});
+            setStockInfo({ price: null, error: 'Error retrieving stock price' });
         }
     };
 
     const handlePin = () => {
-        setPinnedStocks([...pinnedStocks, {symbol: search.toUpperCase(), price: stockInfo.price}]);
+        setPinnedStocks([...pinnedStocks, { symbol: search.toUpperCase(), price: stockInfo.price }]);
+    };
+
+    const handleRemove = (index) => {
+        setPinnedStocks(pinnedStocks.filter((_, i) => i !== index));
     };
 
     return (
-        <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', gap: '1rem'}}>
-            <MDBCard style={{maxWidth: '25%', minHeight: '400px', marginTop: '1rem'}}>
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', gap: '1rem' }}>
+            <MDBCard style={{ maxWidth: '25%', minHeight: '400px', marginTop: '1rem' }}>
                 <MDBCardImage
                     src='https://mdbootstrap.com/img/new/standard/nature/184.webp'
                     position='top'
                     alt='...'
                 />
-                <MDBCardBody style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+                <MDBCardBody style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                     <div>
                         <MDBCardTitle>Stock Price Search</MDBCardTitle>
                         <MDBInput
@@ -54,7 +71,9 @@ export default function StocksCard() {
                             <MDBCardText>
                                 The current price of {search.toUpperCase()} stock is: ${stockInfo.price}
                             </MDBCardText>
-                            <MDBBtn onClick={handlePin}>Pin Stock</MDBBtn>
+                            <MDBBtn onClick={handlePin}>
+                                <MDBIcon fas icon='thumbtack' /> Pin Stock
+                            </MDBBtn>
                         </>
                     )}
                     {stockInfo.error && (
@@ -65,14 +84,17 @@ export default function StocksCard() {
                 </MDBCardBody>
             </MDBCard>
             {pinnedStocks.map((stock, index) => (
-                <MDBCard key={index} style={{maxWidth: '25%', minHeight: '400px', marginTop: '1rem', padding: '0.5rem'}}>
-                    <MDBCardBody style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                        <div style={{marginBottom: 'auto'}}>
+                <MDBCard key={index} style={{ maxWidth: '25%', minHeight: '400px', marginTop: '1rem', padding: '0.5rem' }}>
+                    <MDBCardBody style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                        <div style={{ marginBottom: 'auto' }}>
                             <MDBCardTitle>{stock.symbol}</MDBCardTitle>
-                            <MDBCardText style={{marginBottom: 0}}>
+                            <MDBCardText style={{ marginBottom: 0 }}>
                                 The current price of {stock.symbol} stock is: ${stock.price}
                             </MDBCardText>
                         </div>
+                        <MDBBtn color='danger' onClick={() => handleRemove(index)}>
+                            Remove
+                        </MDBBtn>
                     </MDBCardBody>
                 </MDBCard>
             ))}
